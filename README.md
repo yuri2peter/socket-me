0ceb0ff6f326ed10348b5f194936eafbf8425005
yuri2 <yuri2peter@qq.com>
2022-07-03 11:47:59 +0800

    完善服务端

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # socket-me
-一个支持开放socketio客户端协议的服务端，很适合作为其他小的联机应用的公共服务器。
+
+一个支持开放 socketio 客户端协议的服务端，很适合作为其他小的联机应用的公共服务器。
+
+## SocketIO Versions
+
+- 服务端：4.5.1
+- 客户端：4.5.0
+
+对于客户端，推荐使用 CDN：
+
+```html
+<script
+  src="https://cdn.socket.io/4.5.0/socket.io.min.js"
+  integrity="sha384-7EyYLQZgWBi67fBtVxw60/OWl1kjsfrPFcaU0pp0nAh+i8FD068QogUvg85Ewy1k"
+  crossorigin="anonymous"
+></script>
+```
+
+## Quick Start
+
+```html
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script
+      src="https://cdn.socket.io/4.5.0/socket.io.min.js"
+      integrity="sha384-7EyYLQZgWBi67fBtVxw60/OWl1kjsfrPFcaU0pp0nAh+i8FD068QogUvg85Ewy1k"
+      crossorigin="anonymous"
+    ></script>
+    <title>socket-me</title>
+  </head>
+  <body>
+    <div></div>
+  </body>
+  <script>
+    (() => {
+      // replace <server-host> with true hostname.
+      const socket = io('http://<server-host>');
+      socket.on('connect', () => {
+        console.log('socket connected.');
+        socket.emit('join', {
+          room: 'room-1',
+          name: 'yuri',
+        });
+        socket.emit('speak', {
+          to: { name: 'yuri' },
+          data: { msg: 'I am trying to speak to myself.' },
+        });
+      });
+      socket.on('speak', eventData => {
+        console.log(eventData);
+      });
+      socket.on('disconnect', () => {
+        console.log('socket disconnected.');
+      });
+    })();
+  </script>
+</html>
+```
--- src/markdown/ioServer.js
+++ src/markdown/ioServer.js
@@ -1,8 +1,112 @@
+const users = [];
+
 function ioServer(io) {
-  console.log('io server started');
+  console.log('io server started.');
   io.on('connection', socket => {
-    console.log('a user connected');
+    const { id } = socket;
+    devOnly(() => {
+      console.log(`[${id}] connected.`);
+    });
+
+    socket.on('disconnect', () => {
+      devOnly(() => {
+        console.log(`[${id}] disconnected.`);
+      });
+
+      removeUser(id);
+    });
+
+    /**
+     * 客户端声明自己加入的room和自有属性
+     */
+    socket.on('join', ({ room, ...props }) => {
+      try {
+        devOnly(() => {
+          console.log(`[${id}] joined ${room} with ${JSON.stringify(props)}.`);
+        });
+        upsertUser(id, {
+          room,
+          props,
+          socket,
+        });
+      } catch (error) {
+        devOnly(() => {
+          console.error(error);
+        });
+      }
+    });
+
+    /**
+     * 服务端广播客户端的信息，通过to和room筛选需要发送的对象
+     */
+    socket.on('speak', ({ to = {}, data = {} }) => {
+      try {
+        const user = findUserById(id);
+        if (!user || !user.room) {
+          devOnly(() => {
+            console.log(`[${id}] try to speak without a specific room.`);
+          });
+
+          return;
+        }
+        const { room } = user;
+        devOnly(() => {
+          console.log(
+            `[${id}] in ${room} speak to ${JSON.stringify(
+              to
+            )}: ${JSON.stringify(data)}.`
+          );
+        });
+
+        const matches = users.filter(t => {
+          if (t.room !== room) {
+            return false;
+          }
+          for (const k of Object.keys(to)) {
+            if (t.props[k] !== to[k]) {
+              return false;
+            }
+          }
+          return true;
+        });
+        const eventData = {
+          from: user.id,
+          to,
+          data,
+        };
+        matches.forEach(m => {
+          m.socket.emit('speak', eventData);
+        });
+      } catch (error) {
+        devOnly(() => {
+          console.error(error);
+        });
+      }
+    });
   });
+}
+
+function upsertUser(id, data = {}) {
+  removeUser(id);
+  users.push({ id, ...data });
+}
+
+function removeUser(id) {
+  const prevIndex = users.findIndex(user => user.id === id);
+  if (prevIndex >= 0) {
+    users.splice(prevIndex, 1);
+  }
+}
+
+function findUserById(id) {
+  return users.find(user => user.id === id);
+}
+
+function devOnly(action) {
+  if (process.env.NODE_ENV === 'production') {
+    return;
+  }
+  action();
 }
 
 module.exports = ioServer;
